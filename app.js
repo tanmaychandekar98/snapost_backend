258d5a52056cf6cd183503fbc2a4f089e656eed6
tchandek <tchandek@cisco.com>
2020-05-03 13:29:56 +0530

    add cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const createError = require('http-errors');
+const cors = require('cors')
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -10,6 +11,7 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
