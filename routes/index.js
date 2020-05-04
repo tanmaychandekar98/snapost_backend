db5aef9cfad3dd9e14ea7757d238ea52ae5a2855
tchandek <tchandek@cisco.com>
2020-05-04 22:09:38 +0530

    Add like function

--- routes/index.js
+++ routes/index.js
@@ -3,39 +3,86 @@ const multer  =   require('multer');
 const path = require('path');
 const router = express.Router();
 const fs = require('fs');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
+// Configure local storage of images
 const storage =   multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, './data/uploads');
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-  }
+	destination: function (req, file, callback) {
+		callback(null, './data/uploads');
+	},
+	filename: function (req, file, callback) {
+		callback(null, req.body.userId + '-' + file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+	}
 });
-const upload = multer({ storage : storage}).any('selectedImage');
+const upload = multer({ storage : storage});
+
+// Configure database
+const adapter = new FileSync('./data/db.json')
+const db = low(adapter)
+const Users = db.get('users');
+const Images = db.get('images');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendfile('./index.html');
+	res.sendfile('./index.html');
+	console.log(db.get('users').find({userId : 'tanmay'}).value());
 });
 
-router.get('/api/images', function(req, res, next) {
-	files = fs.readdirSync('data/uploads');
-  console.log(files);
-  res.json({ imgUrls : files});
+
+// Get all image urls
+router.get('/images/urls', function(req, res, next) {
+	console.log('Request received for image URLs fetch');
+
+	res.json({ imgUrls : Images.map('imgName').value()});
 });
 
-router.post('/api/photo/upload',function(req,res){
-	 console.log(req.body)
-    upload(req,res,function(err) {
-        if(err) {
-        	console.log(err)
-            return res.end("Error uploading file.");
-        }
-        res.end("File is uploaded");
-    });
+
+// Request for image upload
+router.post('/image/upload', upload.single("selectedImage"), function(req,res){
+ 	console.log('Request received for uploading image')
+
+	if(db.get('users').find({userId : req.body.userId}).value()) {
+
+		// store image locally
+		Users.find({ userId: req.body.userId})
+			.get('images')
+			.push(req.file.filename)
+			.write();
+
+		Images.push(getImageObject(req))
+			.write();
+
+		res.json({"msg" : "Upload success"});
+
+	} else {
+
+		console.log('Invalid user !');
+		res.json({"msg" : "Invalid user ! Upload failed"});
+		fs.unlinkSync(req.file.path);
+		console.log('Uploaded Image deleted')
+	}
 });
 
+router.post('/image/like', function (req, res) {
+	console.log('Like recieved by server')
+
+	old_likes = Images.find({ imgName : req.body.imgName }).value().likes
+	console.log(old_likes)
+	Images.find({ imgName : req.body.imgName })
+		.assign({ likes : old_likes+1 })
+		.write();
+
+	res.json({"msg" : "Like added"});
+});
+
+function getImageObject(req) {
+	return {
+		imgName : req.file.filename,
+		userId : req.body.userId,
+		likes : 0
+	}
+}
 
 module.exports = router;
